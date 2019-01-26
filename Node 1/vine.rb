66c365acbe3a6a5642cd327cecd9e9529c1a9fa8
James Gromis <dagwoodsandwich56@gmail.com>
2019-01-26 17:06:56 +0100

    Create vine.rb

---
+++ Node 1/vine.rb
@@ -0,0 +1,35 @@
+require 'colorize'
+
+a = 1
+
+while a == 1 do
+print '0'
+puts '0 0 0 0 0'.green
+print '0'
+puts ' 0 0 0 0 0'.blue
+print '0'
+puts '  0 0 0 0 0'.red
+print '0'
+puts '   0 0 0 0 0'.green
+print '0'
+puts '    0 0 0 0 0'.blue
+print '0'
+puts '     0 0 0 0 0'.red
+print '0'
+puts '    0 0 0 0 0'.green
+print '0'
+puts '   0 0 0 0 0'.blue
+print '0'
+puts '  0 0 0 0 0'.red
+print '0'
+puts ' 0 0 0 0 0'.green
+print '0'
+puts '  0 0 0 0 0'.blue
+print '0'
+puts '   0 0 0 0 0'.red
+print '0'
+puts '    0 0 0 0 0'.green
+print '0'
+puts '     0 0 0 0 0'.blue
+print '0'
+end
